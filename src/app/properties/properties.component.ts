65ac43098d14fc79df4b54b7c9bfbd060b5cb2a6
Santanu Mudi <santanumudi8@gmail.com>
2024-01-02 01:03:05 +0530

    added functionality

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PropertyService } from '../shared/property.service';
+import { Property } from './properties.model';
 
 @Component({
   selector: 'app-properties',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./properties.component.css']
 })
 export class PropertiesComponent {
+
+  allProperty:any;
+  formValue!:FormGroup;
+  propertyModelObj:Property = new Property();
+  showAdd!:boolean;
+  showEdit!:boolean;
+
+  constructor(private fb : FormBuilder, private api : PropertyService){ }
+
+  ngOnInit():void{
+    this.formValue = this.fb.group({
+      ptitle:[''],
+      pprice:[''],
+      plocation:[''],
+      pdetails:['']
+    })
+    this.getAllProperty();
+  }
+
+  clickAddProperty(){
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showEdit = false;
+  }
+
+  //get all
+  getAllProperty(){
+    this.api.getAllProp().subscribe((res)=>{
+      this.allProperty = res;
+      console.warn(this.allProperty)
+    })
+  }
+
+  //add prop
+  addProp(){
+    this.propertyModelObj.ptitle = this.formValue.value.ptitle
+    this.propertyModelObj.pprice = this.formValue.value.pprice
+    this.propertyModelObj.plocation = this.formValue.value.plocation
+    this.propertyModelObj.pdetails = this.formValue.value.pdetails
+    this.api.addListing(this.propertyModelObj).subscribe((res)=>{
+      console.log(res)
+      alert("Record added successfully!!")
+      let ref = document.getElementById('clear')
+      ref?.click()
+      this.formValue.reset()
+      this.getAllProperty()
+    }, err=>{
+      alert("Something went wrong")
+    })
+  }
+
+
+  deleteProp(data:any){
+    this.api.deleteProp(data.id).subscribe((res)=>{
+      alert('Property Record Deleted Successfull!!')
+      this.getAllProperty();
+    })
+  }
 
 }
---
+++ src/app/properties/properties.model.ts
@@ -0,0 +1,8 @@
+export class Property{
+    id:number=0;
+    ptitle:string='';
+    pprice:string='';
+    plocation:string='';
+    pdetails:string='';
+
+}
